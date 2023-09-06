bc2726347fd0eb3e6bfd97f49eb52d9c45a2bbc8
Seweryn Świrgoń <ssevve@gmail.com>
2023-09-06 11:06:47 +0200

    Remove separate DesktopNavDropdown component file

--- src/components/DesktopNav.tsx
+++ src/components/DesktopNav.tsx
@@ -1,5 +1,52 @@
-import DesktopNavDropdown from '@/components/DesktopNavDropdown';
+'use client';
+
+import { ChevronDown } from 'lucide-react';
+import Link from 'next/link';
+import { useState } from 'react';
+
+import { Button } from '@/components/ui/Button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/DropdownMenu';
+import { NavOption } from '@/lib/constants';
 import { navOptions } from '@/lib/constants';
+import { cn } from '@/lib/utils';
+
+interface DesktopNavDropdownProps {
+  option: NavOption;
+}
+
+function DesktopNavDropdown({ option }: DesktopNavDropdownProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <DropdownMenu onOpenChange={() => setIsOpen((prev) => !prev)}>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost">
+          <span className="mr-1">{option.label}</span>
+          <ChevronDown
+            className={cn(
+              'rotate-0 transition-transform duration-100',
+              isOpen && 'rotate-180'
+            )}
+            size={15}
+          />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        {option.children.map((child) => (
+          <DropdownMenuItem key={child.href}>
+            <Link href={`${option.path}/${child.href}`} className="text-sm">
+              {child.label}
+            </Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
 
 export default function DesktopNavigation() {
   return (

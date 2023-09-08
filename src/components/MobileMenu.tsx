f55784f9d8e9735465ff2c09d756310b40a0a6bb
Seweryn Świrgoń <ssevve@gmail.com>
2023-09-08 12:10:08 +0200

    Add NavOption component to MobileMenu

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,8 +1,39 @@
 import Link from 'next/link';
 
 import useLockedBody from '@/hooks/useLockedBody';
-import { navOptions } from '@/lib/constants';
+import { NavOption, navOptions } from '@/lib/constants';
 import { cn } from '@/lib/utils';
+
+import { buttonVariants } from './ui/Button';
+
+interface NavOptionProps {
+  option: NavOption;
+}
+
+function NavOption({ option }: NavOptionProps) {
+  return (
+    <div>
+      <h2 className="mb-2 ml-4 text-lg font-semibold tracking-tight text-primary">
+        {option.label}
+      </h2>
+      <ul className="mb-8 grid">
+        {option.children.map((child) => (
+          <li key={child.href}>
+            <Link
+              className={cn(
+                buttonVariants({ variant: 'ghost' }),
+                'w-full justify-start'
+              )}
+              href={`${option.path}/${child.href}`}
+            >
+              {child.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default function MobileMenu() {
   useLockedBody();
@@ -16,24 +47,7 @@ export default function MobileMenu() {
         )}
       >
         {navOptions.map((option) => (
-          <>
-            <h2 className="mb-2 mt-8 text-lg font-semibold tracking-tight text-primary first:mt-0">
-              {option.label}
-            </h2>
-            <ul>
-              <li className="space-y-1">
-                {option.children.map((child) => (
-                  <Link
-                    key={child.href}
-                    href={`${option.path}/${child.href}`}
-                    className="block w-full justify-start hover:underline"
-                  >
-                    {child.label}
-                  </Link>
-                ))}
-              </li>
-            </ul>
-          </>
+          <NavOption key={option.path} option={option} />
         ))}
       </div>
     </div>

4193ed2d7d5e716a9878f011269401e3c48cf8f4
Seweryn Świrgoń <ssevve@gmail.com>
2023-09-17 13:33:50 +0200

    Add margin top to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import Logo from './Logo';
 
 export default function Footer() {
   return (
-    <footer className="rounded-md border-t p-4">
+    <footer className="mt-8 rounded-md border-t p-4">
       <div className="container mx-auto w-full divide-y py-4">
         <div>
           <Link href="/">

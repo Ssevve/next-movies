da3897e2a3ed4236287a432062255d63645a7562
Seweryn Świrgoń <ssevve@gmail.com>
2023-10-10 21:54:00 +0200

    Update getVideos tests

--- src/services/tmdb/api/getVideos/getVideos.test.ts
+++ src/services/tmdb/api/getVideos/getVideos.test.ts
@@ -12,56 +12,72 @@ import Video from '@/types/Video';
 
 const endpoint = `${TMDB_BASE_URL}/:showType/:showId/videos`;
 
-type ShowArgs = Pick<Video, 'showId' | 'showTitle' | 'showType'>;
-
-const movieArgs: ShowArgs = {
-  showId: 1,
-  showTitle: 'Test Movie Title',
-  showType: 'movie',
-};
-
-const tvShowArgs: ShowArgs = {
-  showId: 2,
-  showTitle: 'Test Tv Show Title',
-  showType: 'tv',
-};
-
-const expectedTMDBThumbnailPath = '/testPath';
+type ShowProps = Pick<Video, 'showId' | 'showTitle' | 'showType'>;
 
 describe('getVideos', () => {
   it('should return correct results for movies', async () => {
+    const expectedTMDBThumbnailPath = '/testPath';
+    const expectedShowProps: ShowProps = {
+      showId: 1,
+      showTitle: 'Test Movie Title',
+      showType: 'movie',
+    };
     const expectedVideos = transformVideos({
       videos: mockTMDBMovieVideos,
-      ...movieArgs,
+      ...expectedShowProps,
       thumbnailPath: expectedTMDBThumbnailPath,
       thumbnailSource: 'TMDB',
     });
-    const response = await getVideos({ ...movieArgs, thumbnailPath: expectedTMDBThumbnailPath });
+
+    const response = await getVideos({
+      ...expectedShowProps,
+      thumbnailPath: expectedTMDBThumbnailPath,
+    });
     expect(response).toEqual(expectedVideos);
   });
 
   it('should return correct results for TV shows', async () => {
+    const expectedTMDBThumbnailPath = '/testPath';
+    const expectedShowProps: ShowProps = {
+      showId: 2,
+      showTitle: 'Test Tv Show Title',
+      showType: 'tv',
+    };
     const expectedVideos = transformVideos({
       videos: mockTMDBTvShowVideos,
-      ...tvShowArgs,
+      ...expectedShowProps,
       thumbnailPath: expectedTMDBThumbnailPath,
       thumbnailSource: 'TMDB',
     });
-    const response = await getVideos({ ...tvShowArgs, thumbnailPath: expectedTMDBThumbnailPath });
+
+    const response = await getVideos({
+      ...expectedShowProps,
+      thumbnailPath: expectedTMDBThumbnailPath,
+    });
     expect(response).toEqual(expectedVideos);
   });
 
   it('should return empty array if no videos found', async () => {
+    const expectedShowProps: ShowProps = {
+      showId: 1,
+      showTitle: 'Test Movie Title',
+      showType: 'movie',
+    };
     server.use(
       rest.get(endpoint, (req, res, ctx) => {
         return res(ctx.status(200), ctx.json([]));
       })
     );
-    const response = await getVideos({ ...movieArgs, thumbnailPath: expectedTMDBThumbnailPath });
+    const response = await getVideos({ ...expectedShowProps, thumbnailPath: '' });
     expect(response).toEqual([]);
   });
 
   it('should throw correct error on failed fetch for movies', () => {
+    const expectedShowProps: ShowProps = {
+      showId: 1,
+      showTitle: 'Test Movie Title',
+      showType: 'movie',
+    };
     server.use(
       rest.get(endpoint, (req, res, ctx) => {
         return res(ctx.status(500));
@@ -70,11 +86,16 @@ describe('getVideos', () => {
 
     const showId = 1;
     expect(async () => {
-      await getVideos({ ...movieArgs, thumbnailPath: expectedTMDBThumbnailPath });
+      await getVideos({ ...expectedShowProps, thumbnailPath: '' });
     }).rejects.toThrow(`Failed to fetch videos for: ${showId}.`);
   });
 
   it('should throw correct error on failed fetch for TV shows', () => {
+    const expectedShowProps: ShowProps = {
+      showId: 2,
+      showTitle: 'Test Tv Show Title',
+      showType: 'tv',
+    };
     server.use(
       rest.get(endpoint, (req, res, ctx) => {
         return res(ctx.status(500));
@@ -83,7 +104,7 @@ describe('getVideos', () => {
 
     const showId = 2;
     expect(async () => {
-      await getVideos({ ...tvShowArgs, thumbnailPath: expectedTMDBThumbnailPath });
+      await getVideos({ ...expectedShowProps, thumbnailPath: '' });
     }).rejects.toThrow(`Failed to fetch videos for: ${showId}.`);
   });
 });

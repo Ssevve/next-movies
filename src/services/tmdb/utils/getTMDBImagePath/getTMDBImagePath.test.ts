414c5ef425763a00980ffd252edefe8326d763f7
Seweryn Świrgoń <ssevve@gmail.com>
2023-10-20 19:39:14 +0200

    Handle empty image string in getTMDBImagePath

--- src/services/tmdb/utils/getTMDBImagePath/getTMDBImagePath.test.ts
+++ src/services/tmdb/utils/getTMDBImagePath/getTMDBImagePath.test.ts
@@ -6,13 +6,17 @@ describe('getTMDBImagePath', () => {
     const expectedWidth = 200;
     const expectedHeight = 250;
     const expectedImage = '/testImage.jpg';
-    expect(getTMDBImagePath(expectedImage, expectedWidth, expectedHeight)).toEqual(
+    expect(getTMDBImagePath(expectedImage, expectedWidth, expectedHeight)).toBe(
       `${TMDBUrls.image}/w${expectedWidth}_and_h${expectedHeight}_face${expectedImage}`
     );
   });
 
   it('should return correct string when height and width are omitted', () => {
     const expectedImage = '/testImage.jpg';
-    expect(getTMDBImagePath(expectedImage)).toEqual(`${TMDBUrls.image}/original${expectedImage}`);
+    expect(getTMDBImagePath(expectedImage)).toBe(`${TMDBUrls.image}/original${expectedImage}`);
+  });
+
+  it('should return correct empty string if image string is empty', () => {
+    expect(getTMDBImagePath('')).toBe('');
   });
 });
--- src/services/tmdb/utils/getTMDBImagePath/getTMDBImagePath.ts
+++ src/services/tmdb/utils/getTMDBImagePath/getTMDBImagePath.ts
@@ -1,6 +1,7 @@
 import { TMDBUrls } from '@/services/TMDB/config';
 
 export default function getTMDBImagePath(image: string, width?: number, height?: number) {
+  if (!image) return '';
   if (!width || !height) return `${TMDBUrls.image}/original${image}`;
   else return `${TMDBUrls.image}/w${width}_and_h${height}_face${image}`;
 }

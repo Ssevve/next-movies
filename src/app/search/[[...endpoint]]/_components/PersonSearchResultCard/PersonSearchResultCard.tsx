6ff66d5d654ea618825f71cb09f8f003e53e549e
Seweryn Świrgoń <ssevve@gmail.com>
2023-10-26 12:30:57 +0200

    Extract PersonSearchResultCard and ShowSearchResultCard components to separate files

---
+++ src/app/search/[[...endpoint]]/_components/PersonSearchResultCard/PersonSearchResultCard.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link';
+
+import PersonImage from '@/components/PersonImage/PersonImage';
+import PersonSearchResult from '@/types/PersonSearchResult';
+
+// TODO: tests
+export default function PersonSearchResultCard({
+  name,
+  imagePath,
+  department,
+  shows,
+}: PersonSearchResult) {
+  return (
+    <div className="flex flex-col items-center gap-4 sm:flex-row sm:items-start">
+      <PersonImage imagePath={imagePath} alt={name} />
+      <div className="flex flex-col items-center gap-2 sm:items-start">
+        <div>
+          <h3 className="font-bold">{name}</h3>
+          <span className="block w-full text-center text-sm text-slate-400 sm:text-left">
+            {department}
+          </span>
+        </div>
+        <div className="flex flex-col items-center gap-1 sm:items-start">
+          <span className="mr-1 text-sm font-semibold">Known for:</span>
+          <div className="flex flex-col flex-wrap text-center sm:flex-row sm:text-left">
+            {shows.map(({ id, showType, title }, index) => (
+              <Link
+                key={id}
+                href={`/${showType}/${id}`}
+                className="mr-1 leading-none hover:underline"
+              >
+                <span className="text-xs italic">{`${title}${
+                  index < shows.length - 1 ? ',' : ''
+                }`}</span>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/search/[[...endpoint]]/_components/SearchResults/SearchResults.tsx
+++ src/app/search/[[...endpoint]]/_components/SearchResults/SearchResults.tsx
@@ -1,11 +1,10 @@
-import Link from 'next/link';
-
-import PersonImage from '@/components/PersonImage/PersonImage';
-import ShowCard from '@/components/ShowCard/ShowCard';
 import getSearchResults from '@/services/TMDB/api/getSearchResults/getSearchResults';
 import PersonSearchResult from '@/types/PersonSearchResult';
 import SearchEndpoint from '@/types/SearchEndpoint';
 import ShowSearchResult from '@/types/ShowSearchResult';
+
+import PersonSearchResultCard from '../PersonSearchResultCard/PersonSearchResultCard';
+import ShowSearchResultCard from '../ShowSearchResultCard/ShowSearchResultCard';
 
 function isShowSearchResult(
   result: ShowSearchResult | PersonSearchResult
@@ -19,7 +18,7 @@ interface SearchResultsProps {
   query?: string;
 }
 
-// TODO: tests, fix spacing
+// TODO: tests
 export default async function SearchResults({ page, endpoint, query }: SearchResultsProps) {
   const paginatedResults = await getSearchResults({
     endpoint: endpoint || 'movie',
@@ -29,68 +28,29 @@ export default async function SearchResults({ page, endpoint, query }: SearchRes
 
   return (
     <ul className="grid-cols-fluid mt-12 grid gap-8 divide-y sm:mt-0 sm:grid-cols-1 lg:grid-cols-2">
-      {paginatedResults.results.map((result) => {
-        if (isShowSearchResult(result)) {
-          return (
-            <li key={result.id} className="pt-8 first:border-t last:border-b">
-              <div className="flex flex-col items-center gap-2 sm:flex-row sm:items-start">
-                <ShowCard
-                  id={result.id}
-                  poster={result.poster}
-                  releaseDate={result.releaseDate}
-                  showType={result.showType}
-                  title={result.title}
-                  userScore={result.userScore}
-                  showMetadata={false}
-                />
-                <div className="flex flex-1 flex-col gap-4 px-2">
-                  <div className="flex flex-col items-center gap-1 sm:items-start">
-                    <Link href={`/${result.showType}/${result.id}`}>
-                      <h3 className="text-center font-bold sm:text-left">{result.title}</h3>
-                    </Link>
-                    <span className="text-xs text-slate-400">{result.releaseDate}</span>
-                  </div>
-                  <p className="my-auto line-clamp-4 overflow-hidden text-ellipsis text-center text-sm leading-normal sm:text-left">
-                    {result.overview}
-                  </p>
-                </div>
-              </div>
-            </li>
-          );
-        } else {
-          return (
-            <li key={result.id} className="pt-8 first:border-t last:border-b">
-              <div className="flex flex-col items-center gap-4 sm:flex-row sm:items-start">
-                <PersonImage imagePath={result.imagePath} alt={result.name} />
-                <div className="flex flex-col items-center gap-2 sm:items-start">
-                  <div>
-                    <h3 className="font-bold">{result.name}</h3>
-                    <span className="block w-full text-center text-sm text-slate-400 sm:text-left">
-                      {result.department}
-                    </span>
-                  </div>
-                  <div className="flex flex-col items-center gap-1 sm:items-start">
-                    <span className="mr-1 text-sm font-semibold">Known for:</span>
-                    <div className="flex flex-col flex-wrap text-center sm:flex-row sm:text-left">
-                      {result.shows.map(({ id, showType, title }, index) => (
-                        <Link
-                          key={id}
-                          href={`/${showType}/${id}`}
-                          className="mr-1 leading-none hover:underline"
-                        >
-                          <span className="text-xs italic">{`${title}${
-                            index < result.shows.length - 1 ? ',' : ''
-                          }`}</span>
-                        </Link>
-                      ))}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </li>
-          );
-        }
-      })}
+      {paginatedResults.results.map((result) => (
+        <li key={result.id} className="pt-8 first:border-t last:border-b">
+          {isShowSearchResult(result) ? (
+            <ShowSearchResultCard
+              id={result.id}
+              overview={result.overview}
+              poster={result.poster}
+              releaseDate={result.releaseDate}
+              showType={result.showType}
+              title={result.title}
+              userScore={result.userScore}
+            />
+          ) : (
+            <PersonSearchResultCard
+              department={result.department}
+              id={result.id}
+              imagePath={result.imagePath}
+              name={result.name}
+              shows={result.shows}
+            />
+          )}
+        </li>
+      ))}
     </ul>
   );
 }

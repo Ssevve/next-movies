bbdb500754e7ae8bbb712c245afe01ff5aa31754
Seweryn Świrgoń <ssevve@gmail.com>
2023-10-27 11:29:56 +0200

    SearchCategories refactor and scrollIntoView

--- src/app/search/[[...endpoint]]/_components/SearchCategories/SearchCategories.tsx
+++ src/app/search/[[...endpoint]]/_components/SearchCategories/SearchCategories.tsx
@@ -8,7 +8,13 @@ import { ScrollArea, ScrollBar } from '@/components/ui/ScrollArea';
 import cn from '@/lib/cn';
 import SearchEndpoint from '@/types/SearchEndpoint';
 
-// TODO: tests, scroll into view
+interface Category {
+  endpoint: SearchEndpoint;
+  label: string;
+  total: number;
+}
+
+// TODO: tests
 export default function SearchCategories({
   totalMovies,
   totalTvShows,
@@ -20,25 +26,36 @@ export default function SearchCategories({
 }) {
   const params = useParams();
   const searchParams = useSearchParams();
-  const moviesRef = useRef<HTMLAnchorElement>(null);
-  const tvShowsRef = useRef<HTMLAnchorElement>(null);
-  const peopleRef = useRef<HTMLAnchorElement>(null);
+  const listRef = useRef<HTMLUListElement>(null);
 
-  const currentEndpoint = params.endpoint?.[0] || 'movie';
+  const endpoints: Category[] = [
+    {
+      endpoint: 'movie',
+      label: 'Movies',
+      total: totalMovies,
+    },
+    {
+      endpoint: 'tv',
+      label: 'TV Shows',
+      total: totalTvShows,
+    },
+    {
+      endpoint: 'person',
+      label: 'People',
+      total: totalPeople,
+    },
+  ];
 
-  const refMap = {
-    movie: moviesRef,
-    person: peopleRef,
-    tv: tvShowsRef,
-  };
+  const activeEndpoint = params.endpoint?.[0] || 'movie';
 
-  // useEffect(() => {
-  //   refMap[currentEndpoint].current.scrollIntoView({
-  //     behavior: 'smooth',
-  //     block: 'center',
-  //     inline: 'start',
-  //   });
-  // }, [currentEndpoint]);
+  useEffect(() => {
+    if (listRef.current) {
+      const endpointIndex = endpoints.findIndex((endpoint) => endpoint.endpoint === activeEndpoint);
+      console.log(endpointIndex);
+      listRef.current.children.item(endpointIndex)?.scrollIntoView({ block: 'center' });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [activeEndpoint]);
 
   const generateHref = (endpoint: string) =>
     `/search/${endpoint}?query=${searchParams.get('query') || ''}`;
@@ -46,37 +63,28 @@ export default function SearchCategories({
   const getLinkClassName = (linkEndpoint: SearchEndpoint) =>
     cn(
       'p-4 flex gap-4 min-w-max items-center justify-between w-full hover:bg-slate-50 dark:hover:bg-slate-900',
-      linkEndpoint === currentEndpoint &&
+      linkEndpoint === activeEndpoint &&
         'bg-slate-100 dark:bg-slate-700 hover:bg-slate-100 dark:hover:bg-slate-700'
     );
 
   return (
     <div className="h-max rounded-md border border-slate-100 dark:border-slate-700">
       <ScrollArea>
-        <div className="flex h-max justify-between divide-x divide-slate-100 dark:divide-slate-700 sm:flex-col sm:divide-x-0 sm:divide-y md:w-64">
-          <Link ref={moviesRef} className={getLinkClassName('movie')} href={generateHref('movie')}>
-            <span>Movies</span>
-            <span className="min-w-[45px] rounded-full bg-slate-200 px-2 py-1 text-center text-sm dark:bg-slate-800 dark:text-foreground">
-              {totalMovies}
-            </span>
-          </Link>
-          <Link ref={tvShowsRef} className={getLinkClassName('tv')} href={generateHref('tv')}>
-            <span>TV Shows</span>
-            <span className="min-w-[45px] rounded-full bg-slate-200 px-2 py-1 text-center text-sm dark:bg-slate-800 dark:text-foreground">
-              {totalTvShows}
-            </span>
-          </Link>
-          <Link
-            ref={peopleRef}
-            className={getLinkClassName('person')}
-            href={generateHref('person')}
-          >
-            <span>People</span>
-            <span className="min-w-[45px] rounded-full bg-slate-200 px-2 py-1 text-center text-sm dark:bg-slate-800 dark:text-foreground">
-              {totalPeople}
-            </span>
-          </Link>
-        </div>
+        <ul
+          ref={listRef}
+          className="flex h-max justify-between divide-x divide-slate-100 dark:divide-slate-700 sm:flex-col sm:divide-x-0 sm:divide-y md:w-64"
+        >
+          {endpoints.map(({ endpoint, label, total }) => (
+            <li key={endpoint} className="flex-1">
+              <Link className={getLinkClassName(endpoint)} href={generateHref(endpoint)}>
+                <span>{label}</span>
+                <span className="min-w-[45px] rounded-full bg-slate-200 px-2 py-1 text-center text-sm dark:bg-slate-800 dark:text-foreground">
+                  {total}
+                </span>
+              </Link>
+            </li>
+          ))}
+        </ul>
         <ScrollBar orientation="horizontal" />
       </ScrollArea>
     </div>

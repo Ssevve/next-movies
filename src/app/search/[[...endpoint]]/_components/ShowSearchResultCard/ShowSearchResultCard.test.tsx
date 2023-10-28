a3890a8f1c7d96c5c256fe30c45f549c07d72c69
Seweryn Świrgoń <ssevve@gmail.com>
2023-10-28 11:08:20 +0200

    Add ShowSearchResultCard no image tests

--- src/app/search/[[...endpoint]]/_components/ShowSearchResultCard/ShowSearchResultCard.test.tsx
+++ src/app/search/[[...endpoint]]/_components/ShowSearchResultCard/ShowSearchResultCard.test.tsx
@@ -17,34 +17,43 @@ const searchResult: ShowSearchResult = {
   userScore: 5,
 };
 
+const renderShowSearchResultCard = (props?: Partial<ShowSearchResult>) => {
+  return render(<ShowSearchResultCard {...searchResult} {...props} />);
+};
+
 describe('ShowSearchResultCard', () => {
   it('should render show poster', () => {
-    render(<ShowSearchResultCard {...searchResult} />);
+    renderShowSearchResultCard();
     screen.getByAltText(searchResult.title);
   });
 
+  it('should render <NoImage /> component if poster has no path specified', () => {
+    renderShowSearchResultCard({ poster: { height: 300, path: '', width: 200 } });
+    screen.getByTestId('no-image');
+  });
+
   it('should render show user score', () => {
-    render(<ShowSearchResultCard {...searchResult} />);
+    renderShowSearchResultCard();
     screen.getByText(searchResult.userScore);
   });
 
   it('should render title', () => {
-    render(<ShowSearchResultCard {...searchResult} />);
+    renderShowSearchResultCard();
     screen.getByText(searchResult.title);
   });
 
   it('should render releaseDate', () => {
-    render(<ShowSearchResultCard {...searchResult} />);
+    renderShowSearchResultCard();
     screen.getByText(searchResult.releaseDate);
   });
 
   it('should render overview', () => {
-    render(<ShowSearchResultCard {...searchResult} />);
+    renderShowSearchResultCard();
     screen.getByText(searchResult.overview);
   });
 
   it('should render link to the detailed show page', () => {
-    render(<ShowSearchResultCard {...searchResult} />);
+    renderShowSearchResultCard();
     const linkElements = screen.getAllByRole('link');
     linkElements.some((link) => {
       expect(link).toHaveAttribute('href', `/${searchResult.showType}/${searchResult.id}`);

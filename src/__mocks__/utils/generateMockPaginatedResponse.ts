ac9069d1dcaafa55846bd6743031313b53e25aee
Seweryn Świrgoń <ssevve@gmail.com>
2023-11-01 20:25:37 +0100

    Add resultsPerPage to generateMockPaginatedResponse

--- src/__mocks__/utils/generateMockPaginatedResponse.ts
+++ src/__mocks__/utils/generateMockPaginatedResponse.ts
@@ -1,10 +1,13 @@
 import TMDBPaginatedResponse from '@/services/TMDB/types/TMDBPaginatedResponse';
 
-export default function generateMockPaginatedResponse<T>(results: T[]): TMDBPaginatedResponse<T> {
+export default function generateMockPaginatedResponse<T>(
+  results: T[],
+  resultsPerPage = 1
+): TMDBPaginatedResponse<T> {
   return {
     page: 1,
     results,
-    total_pages: 1,
+    total_pages: Math.ceil(results.length / resultsPerPage),
     total_results: results.length,
   };
 }

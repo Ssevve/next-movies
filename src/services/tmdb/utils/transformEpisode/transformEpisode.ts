0fdce22a77af5ae6ca96a9723d74a798c5dfb1cb
Seweryn Świrgoń <ssevve@gmail.com>
2023-11-02 13:12:22 +0100

    Add transformEpisode tests

--- src/services/tmdb/utils/transformEpisode/transformEpisode.ts
+++ src/services/tmdb/utils/transformEpisode/transformEpisode.ts
@@ -2,16 +2,15 @@ import TMDBEpisode from '@/services/TMDB/types/TMDBEpisode';
 import formatDate from '@/services/TMDB/utils/formatDate/formatDate';
 import Episode from '@/types/Episode';
 
-// TODO: tests
 export default function transformEpisode(episode: TMDBEpisode | null): Episode | null {
   if (!episode) return null;
   return {
-    airDate: episode.air_date ? formatDate(episode.air_date) : '',
+    airDate: episode.air_date ? formatDate(episode.air_date) : 'N/A',
     episodeNumber: episode.episode_number,
     episodeType: episode.episode_type,
     id: episode.id,
-    name: episode.name,
     seasonNumber: episode.season_number,
     showId: episode.show_id,
+    title: episode.name,
   };
 }

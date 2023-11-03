8ad526c4f1696cbfd57a5ddadc6dd7a3423ab08b
Seweryn Świrgoń <ssevve@gmail.com>
2023-11-03 16:53:59 +0100

    Add transformMovieCast tests

--- src/services/tmdb/utils/transformDetailedMovie/utils/transformMovieCast/transformMovieCast.ts
+++ src/services/tmdb/utils/transformDetailedMovie/utils/transformMovieCast/transformMovieCast.ts
@@ -1,8 +1,8 @@
 import TMDBMovieCastPerson from '@/services/TMDB/types/TMDBMovieCastPerson';
 import MovieCastPerson from '@/types/MovieCastPerson';
 
-// TODO: tests
 export default function transformMovieCast(cast: TMDBMovieCastPerson[]): MovieCastPerson[] {
+  if (!cast || !cast.length || !Array.isArray(cast)) return [];
   return cast.map(({ character, id, name, profile_path }) => ({
     character,
     id,

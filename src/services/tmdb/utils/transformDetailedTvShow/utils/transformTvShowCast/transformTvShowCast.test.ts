882eb77500762c32636221e64a0912c3955c8315
Seweryn Świrgoń <ssevve@gmail.com>
2023-11-03 16:55:52 +0100

    Check if cast is an array in transformTvShowCast

--- src/services/tmdb/utils/transformDetailedTvShow/utils/transformTvShowCast/transformTvShowCast.test.ts
+++ src/services/tmdb/utils/transformDetailedTvShow/utils/transformTvShowCast/transformTvShowCast.test.ts
@@ -2,56 +2,70 @@ import TMDBTvShowCastPerson from '@/services/TMDB/types/TMDBTvShowCastPerson';
 import transformTvShowCast from '@/services/TMDB/utils/transformDetailedTvShow/utils/transformTvShowCast/transformTvShowCast';
 import TvShowCastPerson from '@/types/TvShowCastPerson';
 
-const mockCast: TMDBTvShowCastPerson[] = [
-  {
-    id: 1,
-    name: 'John Doe',
-    profile_path: '/testProfilePath',
-    roles: [
-      {
-        character: 'Bob',
-      },
-      {
-        character: 'Matt',
-      },
-    ],
-    total_episode_count: 5,
-  },
-  {
-    id: 2,
-    name: 'Jane Doe',
-    profile_path: '/testProfilePath2',
-    roles: [
-      {
-        character: 'Margaret',
-      },
-      {
-        character: 'Angelina',
-      },
-    ],
-    total_episode_count: 2,
-  },
-];
-
 describe('transformTvShowCast', () => {
   it('should return correctly transformed data for single person', () => {
-    const testCastPerson = mockCast.slice(0, 1);
-
-    const expectedCast: TvShowCastPerson[] = [
+    const mockCast = [
       {
-        characters: testCastPerson[0].roles.map(({ character }) => character),
-        id: testCastPerson[0].id,
-        imagePath: testCastPerson[0].profile_path!,
-        name: testCastPerson[0].name,
-        totalEpisodeCount: testCastPerson[0].total_episode_count,
+        id: 1,
+        name: 'John Doe',
+        profile_path: '/testProfilePath',
+        roles: [
+          {
+            character: 'Bob',
+          },
+          {
+            character: 'Matt',
+          },
+        ],
+        total_episode_count: 5,
       },
     ];
 
-    const transformedCast = transformTvShowCast(testCastPerson);
-    expect(transformedCast).toEqual(expectedCast);
+    const expectedCast: TvShowCastPerson[] = [
+      {
+        characters: mockCast[0].roles.map(({ character }) => character),
+        id: mockCast[0].id,
+        imagePath: mockCast[0].profile_path!,
+        name: mockCast[0].name,
+        totalEpisodeCount: mockCast[0].total_episode_count,
+      },
+    ];
+
+    expect(transformTvShowCast(mockCast)).toEqual(expectedCast);
   });
 
   it('should return correctly transformed data for multiple people', () => {
+    const mockCast: TMDBTvShowCastPerson[] = [
+      {
+        id: 1,
+        name: 'John Doe',
+        profile_path: '/testProfilePath',
+        roles: [
+          {
+            character: 'Bob',
+          },
+          {
+            character: 'Matt',
+          },
+        ],
+        total_episode_count: 5,
+      },
+      {
+        id: 2,
+        name: 'Jane Doe',
+        profile_path: '/testProfilePath2',
+        roles: [
+          {
+            character: 'Margaret',
+          },
+          {
+            character: 'Angelina',
+          },
+        ],
+        total_episode_count: 2,
+      },
+    ];
+
     const expectedCast: TvShowCastPerson[] = mockCast.map(
       ({ id, name, roles, total_episode_count, profile_path }) => ({
         characters: roles.map(({ character }) => character),
@@ -61,24 +75,50 @@ describe('transformTvShowCast', () => {
         totalEpisodeCount: total_episode_count,
       })
     );
-    const transformedCast = transformTvShowCast(mockCast);
-    expect(transformedCast).toEqual(expectedCast);
+
+    expect(transformTvShowCast(mockCast)).toEqual(expectedCast);
   });
 
   it('should return correctly transformed data for if profile_path is not defined', () => {
-    const testCastPerson = [{ ...mockCast.slice(0, 1)[0], profile_path: undefined }];
-
-    const expectedCast: TvShowCastPerson[] = [
+    const mockCast = [
       {
-        characters: testCastPerson[0].roles.map(({ character }) => character),
-        id: testCastPerson[0].id,
-        imagePath: '',
-        name: testCastPerson[0].name,
-        totalEpisodeCount: testCastPerson[0].total_episode_count,
+        id: 1,
+        name: 'John Doe',
+        profile_path: undefined,
+        roles: [
+          {
+            character: 'Bob',
+          },
+          {
+            character: 'Matt',
+          },
+        ],
+        total_episode_count: 5,
       },
     ];
 
-    const transformedCast = transformTvShowCast(testCastPerson);
-    expect(transformedCast).toEqual(expectedCast);
+    const expectedCast: TvShowCastPerson[] = [
+      {
+        characters: mockCast[0].roles.map(({ character }) => character),
+        id: mockCast[0].id,
+        imagePath: '',
+        name: mockCast[0].name,
+        totalEpisodeCount: mockCast[0].total_episode_count,
+      },
+    ];
+
+    expect(transformTvShowCast(mockCast)).toEqual(expectedCast);
+  });
+
+  it('should return empty array if cast array is empty', () => {
+    expect(transformTvShowCast([])).toEqual([]);
+  });
+
+  it('should return empty array if cast array is undefined', () => {
+    expect(transformTvShowCast(undefined as unknown as TMDBTvShowCastPerson[])).toEqual([]);
+  });
+
+  it('should return empty array if cast is not an array', () => {
+    expect(transformTvShowCast('notarray' as unknown as TMDBTvShowCastPerson[])).toEqual([]);
   });
 });
--- src/services/tmdb/utils/transformDetailedTvShow/utils/transformTvShowCast/transformTvShowCast.ts
+++ src/services/tmdb/utils/transformDetailedTvShow/utils/transformTvShowCast/transformTvShowCast.ts
@@ -2,6 +2,7 @@ import TMDBTvShowCastPerson from '@/services/TMDB/types/TMDBTvShowCastPerson';
 import TvShowCastPerson from '@/types/TvShowCastPerson';
 
 export default function transformTvShowCast(cast: TMDBTvShowCastPerson[]): TvShowCastPerson[] {
+  if (!cast || !cast.length || !Array.isArray(cast)) return [];
   return cast.map(({ roles, id, name, profile_path, total_episode_count }) => ({
     characters: roles.map(({ character }) => character),
     id,

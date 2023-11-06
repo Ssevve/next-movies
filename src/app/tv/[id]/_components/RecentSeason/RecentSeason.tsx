983e93d3cf825f86af0533a51c6259efe3f8d3e9
Seweryn Świrgoń <ssevve@gmail.com>
2023-11-06 10:51:34 +0100

    Extract sub-components from RecentSeason

--- src/app/tv/[id]/_components/RecentSeason/RecentSeason.tsx
+++ src/app/tv/[id]/_components/RecentSeason/RecentSeason.tsx
@@ -5,6 +5,7 @@ import NoImage from '@/components/NoImage';
 import SectionHeading from '@/components/SectionHeading/SectionHeading';
 import getTMDBImagePath from '@/services/TMDB/utils/getTMDBImagePath/getTMDBImagePath';
 import Episode from '@/types/Episode';
+import ImageType from '@/types/Image';
 import Season from '@/types/Season';
 import getReleaseYear from '@/utils/getReleaseYear/getReleaseYear';
 
@@ -14,62 +15,121 @@ interface RecentSeasonProps {
   episode: Episode | null;
 }
 
+function SeasonPoster({ poster, alt }: { poster?: ImageType; alt: string }) {
+  return (
+    <div className="col-start-1 col-end-1 row-span-2 row-start-1 flex h-full min-w-[100px] max-w-[175px] sm:row-span-3">
+      {poster && poster.path ? (
+        <Image
+          src={getTMDBImagePath(poster.path, poster.width, poster.height)}
+          alt={alt}
+          width={poster.width}
+          height={poster.height}
+          className="object-scale-down"
+        />
+      ) : (
+        <NoImage />
+      )}
+    </div>
+  );
+}
+
+function SeasonMetadata({
+  name,
+  userScore,
+  airDate,
+  episodeCount,
+}: {
+  name: string;
+  userScore: number;
+  airDate: string;
+  episodeCount: number;
+}) {
+  return (
+    <div className="col-span-full col-start-2 row-start-1 flex h-max flex-col gap-2">
+      <h3 className="text-xl font-bold">{name}</h3>
+      <div className="flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="row-start-1 flex w-max items-center justify-center gap-1 rounded-md bg-foreground p-1 text-background">
+          <Star size={16} />
+          <span className="text-sm font-semibold">{userScore}</span>
+        </div>
+        <span className="text-sm font-semibold">{getReleaseYear(airDate)}</span>
+        <span className="relative text-sm font-semibold before:absolute before:left-0 before:top-1/2 before:hidden before:h-1 before:w-1 before:-translate-y-1/2 before:rounded-full before:bg-foreground sm:pl-3 sm:before:block">
+          {episodeCount} episodes
+        </span>
+      </div>
+    </div>
+  );
+}
+
+function RecentEpisode({
+  showEnded,
+  title,
+  seasonNumber,
+  episodeNumber,
+  airDate,
+  episodeType,
+}: {
+  showEnded: boolean;
+  title: string;
+  seasonNumber: number;
+  episodeNumber: number;
+  airDate?: string;
+  episodeType: string;
+}) {
+  const isSeasonFinaleEpisode = episodeType === 'finale';
+
+  return (
+    <div className="col-start-2 row-start-2 flex flex-col gap-1 text-sm sm:flex-row">
+      <div className="h-max sm:flex sm:items-center">
+        <div className="flex flex-col gap-1 sm:flex-row">
+          <span>{showEnded ? 'Last' : 'Next'} episode:</span>
+          <span className="font-semibold">{title}</span>
+          <span>{`(${seasonNumber}x${episodeNumber} - ${airDate})`}</span>
+        </div>
+        {isSeasonFinaleEpisode && (
+          <span className="ml-1 h-max w-max rounded-md bg-foreground p-1 text-xs font-semibold text-background">
+            Finale
+          </span>
+        )}
+      </div>
+    </div>
+  );
+}
+
+function SeasonOverview({ overview }: { overview?: string }) {
+  return (
+    <p className="col-span-full col-start-1 row-start-3 sm:col-start-2 sm:row-start-auto">
+      {overview || 'Overview not available.'}
+    </p>
+  );
+}
+
 // TODO: tests, fix no image size, fix spacing
 export default function RecentSeason({ season, showEnded, episode }: RecentSeasonProps) {
-  const isSeasonFinaleEpisode = episode && episode.episodeType === 'finale';
   return (
     <section>
       <SectionHeading>{showEnded ? 'Last' : 'Current'} Season</SectionHeading>
-      <div className="mt-4 grid grid-cols-2 gap-x-4 gap-y-4 xs:grid-cols-[175px_1fr] sm:auto-rows-auto sm:items-center">
+      <div className="mt-4 grid grid-cols-2 gap-x-4 gap-y-4 xs:grid-cols-[175px_1fr]">
         {season ? (
           <>
-            <div className="col-start-1 col-end-1 row-span-2 row-start-1 flex h-full min-w-[100px] max-w-[175px] sm:row-span-3 ">
-              {season.poster && season.poster.path ? (
-                <Image
-                  src={getTMDBImagePath(
-                    season.poster.path,
-                    season.poster.width,
-                    season.poster.height
-                  )}
-                  alt={season.name}
-                  width={season.poster.width}
-                  height={season.poster.height}
-                  className="object-scale-down"
-                />
-              ) : (
-                <NoImage />
-              )}
-            </div>
-            <div className="col-span-full col-start-2 row-start-1 flex h-max flex-col gap-2 self-center md:self-end">
-              <h3 className="text-xl font-bold">{season.name}</h3>
-              <div className="flex flex-col gap-2 sm:flex-row sm:items-center">
-                <div className="row-start-1 flex w-max items-center justify-center gap-1 rounded-md bg-foreground p-1 text-background">
-                  <Star size={16} />
-                  <span className="text-sm font-semibold">{season.userScore}</span>
-                </div>
-                <span className="text-sm font-semibold">{getReleaseYear(season.airDate)}</span>
-                <span className="relative text-sm font-semibold before:absolute before:left-0 before:top-1/2 before:hidden before:h-1 before:w-1 before:-translate-y-1/2 before:rounded-full before:bg-foreground sm:pl-3 sm:before:block">
-                  {season.episodeCount} episodes
-                </span>
-              </div>
-            </div>
+            <SeasonPoster poster={season.poster} alt={season.name} />
+            <SeasonMetadata
+              name={season.name}
+              airDate={season.airDate}
+              userScore={season.userScore}
+              episodeCount={season.episodeCount}
+            />
             {episode && (
-              <div className="col-start-2 row-start-2 flex flex-col gap-1 text-sm sm:flex-row">
-                <div className="flex flex-col gap-1 sm:flex-row sm:items-center">
-                  <span>{showEnded ? 'Last' : 'Next'} episode:</span>
-                  <span className="font-semibold">{episode.name}</span>
-                  <span>{`(${episode.seasonNumber}x${episode.episodeNumber} - ${episode.airDate})`}</span>
-                </div>
-                {isSeasonFinaleEpisode && (
-                  <span className="w-max rounded-md bg-foreground p-1 text-xs font-semibold text-background">
-                    Finale
-                  </span>
-                )}
-              </div>
+              <RecentEpisode
+                airDate={episode.airDate}
+                episodeNumber={episode.episodeNumber}
+                episodeType={episode.episodeType}
+                seasonNumber={episode.seasonNumber}
+                showEnded={showEnded}
+                title={episode.title}
+              />
             )}
-            <p className="col-span-full col-start-1 row-start-3 sm:col-start-2 md:self-start">
-              {season.overview || 'Overview not available.'}
-            </p>
+            <SeasonOverview overview={season.overview} />
           </>
         ) : (
           <p className="w-max">No season data available.</p>

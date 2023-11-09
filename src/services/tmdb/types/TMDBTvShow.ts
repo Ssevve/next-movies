68b25ce09f6a4f079dd6ed4cdf5d10534267a33c
Seweryn Świrgoń <ssevve@gmail.com>
2023-11-09 12:12:25 +0100

    Remove unnecessary properties from TMDBShow and  TMDBMovie types

--- src/services/tmdb/types/TMDBTvShow.ts
+++ src/services/tmdb/types/TMDBTvShow.ts
@@ -3,6 +3,4 @@ import TMDBShow from '@/services/TMDB/types/TMDBShow';
 export default interface TMDBTvShow extends TMDBShow {
   name: string;
   first_air_date?: string;
-  title?: never;
-  release_date?: never;
 }

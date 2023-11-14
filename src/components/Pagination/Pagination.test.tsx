eb48961f3d6825eb09cd047b5cc3ed4d2818be15
Seweryn Świrgoń <ssevve@gmail.com>
2023-11-14 21:14:23 +0100

    Fix Pagination tests

--- src/components/Pagination/Pagination.test.tsx
+++ src/components/Pagination/Pagination.test.tsx
@@ -4,22 +4,22 @@ import Pagination from '@/components/Pagination/Pagination';
 
 describe('Pagination', () => {
   it('should render correct amount of total pages', () => {
-    render(<Pagination itemsPerPage={10} totalItemCount={100} />);
-    expect(screen.getByText('10')).toBeInTheDocument();
+    render(<Pagination totalItemCount={100} />);
+    expect(screen.getByText('5')).toBeInTheDocument();
   });
 
   it('should render previous page jumper', () => {
-    render(<Pagination itemsPerPage={10} totalItemCount={100} />);
+    render(<Pagination totalItemCount={100} />);
     expect(screen.getByRole('button', { name: /previous page/i })).toBeInTheDocument();
   });
 
   it('should render page input', () => {
-    render(<Pagination itemsPerPage={10} totalItemCount={100} />);
+    render(<Pagination totalItemCount={100} />);
     expect(screen.getByRole('textbox')).toBeInTheDocument();
   });
 
   it('should render next page jumper', () => {
-    render(<Pagination itemsPerPage={10} totalItemCount={100} />);
+    render(<Pagination totalItemCount={100} />);
     expect(screen.getByRole('button', { name: /next page/i })).toBeInTheDocument();
   });
 });

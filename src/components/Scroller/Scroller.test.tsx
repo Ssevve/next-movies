65802303f9169076b582e0dc700e3dd682bdd687
Seweryn Świrgoń <ssevve@gmail.com>
2023-11-16 17:28:18 +0100

    Fix wrong tests

--- src/components/Scroller/Scroller.test.tsx
+++ src/components/Scroller/Scroller.test.tsx
@@ -23,7 +23,7 @@ describe('Scroller', () => {
         ))}
       </Scroller>
     );
-    expect(screen.getAllByTestId('show-card')).toHaveLength(mockShows.length);
+    expect(screen.getAllByRole('listitem')).toHaveLength(mockShows.length);
   });
 
   it('should render all items if limit is 0', () => {
@@ -34,7 +34,7 @@ describe('Scroller', () => {
         ))}
       </Scroller>
     );
-    expect(screen.getAllByTestId('show-card')).toHaveLength(mockShows.length);
+    expect(screen.getAllByRole('listitem')).toHaveLength(mockShows.length);
   });
 
   it('should render correct amount of items if limit is provided', () => {

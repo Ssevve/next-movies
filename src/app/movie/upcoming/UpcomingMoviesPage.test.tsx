ae989d1545357fcf453f3a8def0d9f13461832d3
Seweryn Świrgoń <ssevve@gmail.com>
2023-11-17 15:51:53 +0100

    Add UpcomingMoviesPage test

---
+++ src/app/movie/upcoming/UpcomingMoviesPage.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen, waitFor } from '@testing-library/react';
+
+import UpcomingMoviesPage from '@/app/movie/now-playing/page';
+
+describe('UpcomingMoviesPage', () => {
+  it('should render <PaginatedShows /> component', async () => {
+    render(await UpcomingMoviesPage({ searchParams: { page: '1' } }));
+    await waitFor(() => {
+      screen.getAllByTestId('show-card');
+    });
+  });
+});

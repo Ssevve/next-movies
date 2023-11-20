dd82c66ecb1833c6afcd0a25035826cfd4be7111
Seweryn Świrgoń <ssevve@gmail.com>
2023-11-20 20:15:16 +0100

    Remove comments from ShowFacts

--- src/components/ShowFacts/ShowFacts.tsx
+++ src/components/ShowFacts/ShowFacts.tsx
@@ -5,7 +5,6 @@ export interface ShowFactsProps {
 
 export default function ShowFacts({ status, originalLanguage }: ShowFactsProps) {
   return (
-    // <div className="flex w-full max-w-xl flex-col justify-between gap-8 sm:flex-row">
     <>
       <div>
         <h3 className="font-semibold">Status</h3>
@@ -15,29 +14,6 @@ export default function ShowFacts({ status, originalLanguage }: ShowFactsProps) 
         <h3 className="font-semibold">Original Language</h3>
         <span className="text-sm">{originalLanguage || '-'}</span>
       </div>
-      {/* {type && (
-        <div>
-          <h3 className="font-semibold">Type</h3>
-          <span className="text-sm">{type}</span>
-        </div>
-      )} */}
-      {/* {networks && (
-        <div>
-          <h3 className="font-semibold">{hasMultipleNetworks ? 'Networks' : 'Network'}</h3>
-          {networks.length ? (
-            <ul className="mt-1 flex flex-col gap-2 md:flex-row md:items-center">
-              {networks.map(({ name, id, logoPath }) => (
-                <li key={id}>
-                  <Image src={getTMDBImagePath(logoPath)} alt={name} width={75} height={37.5} />
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <span>-</span>
-          )}
-        </div>
-      )} */}
     </>
-    // </div>
   );
 }
